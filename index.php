c0c5f2baf0342363eee7e892afbd2507fcdb3428
Flametague <56924694+Flametague@users.noreply.github.com>
2022-02-21 18:56:37 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+
+<html>
+<head>
+	<title>Squadron Production</title>
+	<meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/acceuil.css?ver=1.9">
+	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    
+</head>
+
+<body class="background">
+
+<header>
+<main>	
+	<nav class="navbar">
+		<ul>
+			<li><a href="#"> <i class='bx bxs-home'></i> Home</a></li>
+			<li><a href="#"><i class='bx bx-movie'></i> Shutters</a></li>
+			<li><a href="#"> <i class='bx bxs-cart'></i> Shop </a></li>
+			<li><a href="https://discord.gg/TSAtFU9pSY"><i class='bx bxs-message'></i> Contact</a></li>
+		</ul>
+	</nav>			
+</main>
+</header>
+
+
+
+<section>
+<center><h1 class="news">In the news</h1></center>
+
+<center><main>
+<div class="news1"><h3>Trailer of Fivem production Skylife Rp</h3></div>
+<div class="news1"><video id="vid1" width="800px"    loop> <source src="video/skyliferp.avi" ></video></div>
+
+<div class="news2"><h3>Trailer of Fivem production Aurora Rp</h3></div>
+<div class="news2"><video id="vid2" width="800px"  loop> <source src="video/aurorarp.avi" ></video></div>
+</main></center>
+
+</section>
+  
+
+
+</body>
+
+
+<footer class="footer"><main>
+			<nav class="navbar2">
+				<ul>
+					<li><a href="#"> <i class='bx bxs-home'></i> Home</a></li>
+					<li><a href="#"><i class='bx bx-movie'></i> Shutters</a></li>
+					<li><a href="#"><img src=""><i class='bx bxs-cart'></i> Shop</a></li>
+			    	<li><a href="https://discord.gg/TSAtFU9pSY"><i class='bx bxs-message'></i> Contact</a></li>
+				</ul>
+			</nav>
+		
+	<center><div class="copyright">All rights reserved 2021-2022 Squadron Production © Developped by Flametague</div></center>
+</footer>
+
+
+
+</html>
+
+<script> let video = document.getElementById('vid1'); video.addEventListener('mouseover', ()=>{video.play();}); video.addEventListener('mouseout', ()=>{video.pause();}); </script>
+	<script>document.getElementById('vid1').volume=0.20;</script>
+		<script>document.getElementById('vid1').currentTime = 3;</script>
+
+<script> let video2 = document.getElementById('vid2'); video2.addEventListener('mouseover', ()=>{video2.play();}); video2.addEventListener('mouseout', ()=>{video2.pause();}); </script>
+	<script>document.getElementById('vid2').volume=0.20;</script>
+		<script>document.getElementById('vid2').currentTime = 3;</script>
